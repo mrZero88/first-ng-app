9d5530e033ebcf62efe5eb347419883e3c60f531
correia <correia@cssol.de>
2025-02-05 16:36:39 +0100

    Latest changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styles: [`main {padding: 16px}`],
+  styles: [
+    `
+      main {
+        padding: 16px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'first-ng-app';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { GreetingComponent } from "../components/greeting/greeting.component";
+import { GreetingComponent } from '../components/greeting/greeting.component';
 import { CounterComponent } from '../components/counter/counter.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent, CounterComponent, RouterOutlet],
+  imports: [GreetingComponent, CounterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   homeMessage = signal('Hello from home message!');
